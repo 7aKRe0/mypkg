ceed150474dc89c3d6250381a337abbf8df2e837
Ryosuke Naka <uchuutarou114@gmail.com>
2024-11-28 13:34:00 +0900

    lesson10

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'mypkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'talker = mypkg.talker:main',
-            #'listener = mypkg.listener:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
